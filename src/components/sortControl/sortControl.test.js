5b66c4101a50c550f6f9a4c35225527d6f3c4b2a
Debapriya Sen <Debapriya_Sen@Epam.com>
2023-11-15 19:44:21 +0530

    Fixed unit test case

--- src/components/sortControl/sortControl.test.js
+++ src/components/sortControl/sortControl.test.js
@@ -4,21 +4,21 @@ import SortControl from './sortControl';
 
 describe('Test SortControl component', () => {
   test('it renders the SortControl component', () => {
-    render(<SortControl currentSelection="Release Date" handleSelectChange={() => { }} />);
+    render(<SortControl currentSelection="release_date" handleSelectChange={() => { }} />);
     const labelElement = screen.getByText('SORT BY');
     const selectElement = screen.getByRole('combobox');
 
     expect(labelElement).toBeInTheDocument();
     expect(selectElement).toBeInTheDocument();
 
-    expect(selectElement).toHaveValue('Release Date');
+    expect(selectElement).toHaveValue('release_date');
   });
 
   test('it calls the handleSelectChange function on select change', () => {
     const handleSelectChange = jest.fn();
-    render(<SortControl currentSelection="Release Date" handleSelectChange={handleSelectChange} />);
+    render(<SortControl currentSelection="release_date" handleSelectChange={handleSelectChange} />);
     const selectElement = screen.getByRole('combobox');
-    fireEvent.change(selectElement, { target: { value: 'Title' } });
-    expect(handleSelectChange).toHaveBeenCalledWith('Title');
+    fireEvent.change(selectElement, { target: { value: 'title' } });
+    expect(handleSelectChange).toHaveBeenCalledWith('title');
   });
 })
