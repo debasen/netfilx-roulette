becfea296e6a01f08d632327746d9095f819b6bc
Debapriya Sen <Debapriya_Sen@Epam.com>
2024-01-09 11:25:02 +0530

    Setup eslint, husky, precommit hook

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-// src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import App from './App'; 
 
-const App = () => {
-  return <div>Hello, React!</div>;
-};
-
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
